21262d1fb97f18504a9bf1fe2ed307f1e600473e
印佳 <yinjia@jiajiadeMacBook-Pro.local>
2019-08-12 22:19:29 +0800

    ios build fix

--- build/make_ios_luajit.sh
+++ build/make_ios_luajit.sh
@@ -23,16 +23,16 @@ cd luajit-2.1.0b3
 
 XCODEVER=`xcodebuild -version|head -n 1|sed 's/Xcode \([0-9]*\)/\1/g'`
 
-if [ $XCODEVER -lt 10]
+if [ "$XCODEVER" \< "10" ]
 then
     make clean
     ISDKF="-arch armv7 -isysroot $ISDK/SDKs/$ISDKVER -miphoneos-version-min=7.0"
     make HOST_CC="gcc -m32 -std=c99" TARGET_FLAGS="$ISDKF" TARGET=armv7 TARGET_SYS=iOS LUAJIT_A=libxluav7.a
     
     
-    make clean
-    ISDKF="-arch armv7s -isysroot $ISDK/SDKs/$ISDKVER -miphoneos-version-min=7.0"
-    make HOST_CC="gcc -m32 -std=c99" TARGET_FLAGS="$ISDKF" TARGET=armv7s TARGET_SYS=iOS LUAJIT_A=libxluav7s.a
+    #make clean
+    #ISDKF="-arch armv7s -isysroot $ISDK/SDKs/$ISDKVER -miphoneos-version-min=7.0"
+    #make HOST_CC="gcc -m32 -std=c99" TARGET_FLAGS="$ISDKF" TARGET=armv7s TARGET_SYS=iOS LUAJIT_A=libxluav7s.a
 fi
 
 make clean
@@ -40,7 +40,12 @@ ISDKF="-arch arm64 -isysroot $ISDK/SDKs/$ISDKVER -miphoneos-version-min=7.0"
 make HOST_CC="gcc -std=c99" TARGET_FLAGS="$ISDKF" TARGET=arm64 TARGET_SYS=iOS LUAJIT_A=libxlua64.a
 
 cd src
-lipo libxluav7.a -create libxluav7s.a libxlua64.a -output libluajit.a
+if [ "$XCODEVER" \< "10" ]
+then
+    lipo libxluav7.a -create libxluav7s.a libxlua64.a -output libluajit.a
+else
+    cp -f libxlua64.a libluajit.a
+fi
 cd ../..
 
 mkdir -p build_lj_ios && cd build_lj_ios
